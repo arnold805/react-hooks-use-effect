78151c078b248924128d1cf234d1e1166f67cadf
Greg <garnold.805@gmail.com>
2022-05-02 18:29:00 -0400

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  // side effect will only run the first time component renders
+
+  // useEffect(() => {
+  //   console.log("useEffect called");
+  // }, []); // second argument is an empty array
+
+  // useEffect(
+  //   // 1st arg: side effect (callback function)
+  //   () => console.log("useEffect called"),
+  //   // 2nd arg: dependencies array
+  //   [count]
+  // );
+
+  // update outside of DOM, the document (tab) name
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text]);
+
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
